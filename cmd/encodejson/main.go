9e675ff8081e948a29e26bfd041a92a11155d055
Steve de Silva <7001268+stevedesilva@users.noreply.github.com>
2020-03-17 23:15:40 +0000

    initial commit

---
+++ cmd/encodejson/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type permissions map[string]bool
+
+type user struct {
+	Name, Password string
+	permissions
+}
+
+func main() {
+	users := []user{
+		{"steve", "111", nil},
+		{"clive", "222", permissions{"admin": true}},
+		{"ben", "333", permissions{"write": true}},
+	}
+
+	out, err := json.Marshal(users) // only encodes exported fields
+	if err != nil {
+		fmt.Println("err", err)
+		return
+	}
+	fmt.Println(string(out))
+
+}
