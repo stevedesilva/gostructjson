b75454f93b06aae855c30d78f6eb8842800b8871
Steve de Silva <7001268+stevedesilva@users.noreply.github.com>
2020-03-18 08:54:25 +0000

    added encode and decode

--- cmd/encodejson/main.go
+++ cmd/encodejson/main.go
@@ -1,30 +1,91 @@
 package main
 
 import (
+	"bufio"
 	"encoding/json"
 	"fmt"
+	"os"
 )
 
 type permissions map[string]bool
 
+// ield tags metadata encode and decode field
+// normally a raw string literal since it includes ""
+// key value pair, key is package = json, tag is read by package
 type user struct {
-	name     string
-	password string
-	permissions
+	Name        string      `json:"username"`        // change json display name
+	Password    string      `json:"-"`               // don't encode Password
+	Permissions permissions `json:"perms,omitempty"` // change encoded field to perms, does not encode empty values
 }
 
 func main() {
+
+	// encoder()
+	decoder()
+}
+
+/*
+		[
+	        {
+	                "username": "steve"
+	        },
+	        {
+	                "username": "clive",
+	                "perms": {
+	                        "admin": true
+	                }
+	        },
+	        {
+	                "username": "ben",
+	                "perms": {
+	                        "write": true
+	                }
+	        }
+	]
+*/
+func encoder() {
 	users := []user{
 		{"steve", "111", nil},
 		{"clive", "222", permissions{"admin": true}},
 		{"ben", "333", permissions{"write": true}},
 	}
 
-	out, err := json.Marshal(users) // only encodes exported fields
+	// only encodes exported fields, pretty prints
+
+	out, err := json.MarshalIndent(users, "", "\t")
 	if err != nil {
 		fmt.Println("err", err)
 		return
 	}
 	fmt.Println(string(out))
+}
+
+func decoder() {
+	var input []byte
+
+	for in := bufio.NewScanner(os.Stdin); in.Scan(); {
+		input = append(input, in.Bytes()...)
+	}
+
+	var users []user
+	// Umarshal needs users as a pointer in order to update
+	if err := json.Unmarshal(input, &users); err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	for _, user := range users {
+		fmt.Print("+ " + user.Name)
+		switch p := user.Permissions; {
+		case p == nil:
+			fmt.Print(" has no power")
+		case p["admin"]:
+			fmt.Print(" is an admin")
+		case p["write"]:
+			fmt.Print(" can write")
+
+		}
+		fmt.Println()
+	}
 
 }
