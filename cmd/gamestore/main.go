e5d1962fffb47e095bd045018e2ac60435f33e0e
Steve de Silva <7001268+stevedesilva@users.noreply.github.com>
2020-03-20 07:41:24 +0000

    refactor games

--- cmd/gamestore/main.go
+++ cmd/gamestore/main.go
@@ -15,29 +15,38 @@ func main() {
 	g.Add(2, 30, "x-com 2", "strategy")
 	g.Add(3, 20, "minecraft", "sandbox")
 
-	for _, m := range g.List() {
-		fmt.Println(m)
-	}
-
 	sc := bufio.NewScanner(os.Stdin)
-	for sc.Scan() {
-		in := sc.Text()
 
-		switch {
-		case "Quit" == in:
-			{
-				fmt.Println("Exiting " + in)
-				return
+GamesLoop:
+	for {
+		fmt.Printf(`
+> List  : prints all games
+> Quit  : quit 
+> Search: games list for item
+
+		`)
+
+		if !sc.Scan() {
+			break GamesLoop
+		}
+
+		switch in := sc.Text(); in {
+		case "Quit":
+			fmt.Println("Exiting " + in)
+			return
+
+		case "List":
+			for _, m := range g.List() {
+				fmt.Println(m)
 			}
 
 		default:
-			{
-				if found := g.Search(in); found {
-					fmt.Println("Found " + in)
-				} else {
-					fmt.Println("Not Found " + in)
-				}
+			if found := g.Search(in); found {
+				fmt.Println("Found " + in)
+			} else {
+				fmt.Println("Not Found " + in)
 			}
+
 		}
 
 	}
